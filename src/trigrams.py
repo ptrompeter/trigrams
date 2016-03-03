6640fd7c0a244abe6b1da231fb5d2d1afbdba9ca
Mike <mikekh@uw.edu>
2016-03-02 16:46:15 -0800

    create dict

--- src/trigrams.py
+++ src/trigrams.py
@@ -9,3 +9,11 @@ f = io.open('src/sherlock_small.txt', mode='r')
 testbook = f.read()
 
 wordlst = testbook.split()
+
+
+def create_dict(wordlst):
+	count = 0
+	trigrams = {}
+	for index in range(len(wordlst) - 2):
+		trigrams[(wordlst[index], wordlst[index + 1])] = wordlst[index +2]
+	return trigrams 
