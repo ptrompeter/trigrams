d203ceecaaca35b3a920ffca8f6f78cac7173c98
ptrompeter <patrick.trompeter@gmail.com>
2016-03-02 20:03:42 -0800

    trigrams.py now works super good like a boss

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,19 +1,47 @@
 # _*_ coding: utf-8 _8
 
 import io
+import random
+import math
+import sys
+
+f = io.open(sys.argv[1])
+long_list = f.read().split()
+f.close()
 
 
-f = io.open('src/sherlock_small.txt', mode='r')
+def create_dict(lst):
+    """Generate a trigrams dictionary for a given list."""
+    tri = {}
+    for i in range(len(lst) - 2):
+        tri.setdefault((lst[i], lst[i + 1]), []).append(lst[i + 2])
+    return tri
+
+long_dict = create_dict(long_list)
+
+def make_snippet(lst, dict, sniplen):
+    """Return snippet based upon trigram analysis of source text given snippet length."""
+    rand = math.floor(((len(lst) - 2) * random.random()))
+    snippet = []
+    snippet.append(lst[rand])
+    snippet.append(lst[rand + 1])
+    while (len(snippet) < sniplen):
+        k = (snippet[-2], snippet[-1])
+        snippet.append(
+            dict[k][math.floor(len(dict[k]) * random.random())]
+        )
+    snippet = " ".join(snippet)
+    return snippet
+
+snippet = make_snippet(long_list, long_dict, int(sys.argv[2]))
 
 
-testbook = f.read()
+def output_file(snippet):
+    """Write an imput to src/output.txt."""
+    f = io.open("src/output.txt", mode='w')
+    f.write(snippet)
+    f.close()
 
-wordlst = testbook.split()
-
-
-def create_dict(wordlst):
-	count = 0
-	trigrams = {}
-	for index in range(len(wordlst) - 2):
-		trigrams[(wordlst[index], wordlst[index + 1])] = wordlst[index +2]
-	return trigrams 
+if __name__ == '__main__':
+    print(sys.argv)
+    output_file(snippet)
